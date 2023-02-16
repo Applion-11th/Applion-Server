76569d19646836d330d3c68868137177e4ac92d3
taeho0888 <taeho0888@naver.com>
2023-02-16 20:22:48 +0900

    fix : 지원서 모델에 blank=True 추가

--- application/models.py
+++ application/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 class Application(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Application', primary_key=True)
-    app1 = models.TextField(null=True)
-    app2 = models.TextField(null=True)
-    app3 = models.TextField(null=True)
-    app4 = models.TextField(null=True)
-    github = models.CharField(max_length=100, null=True)
+    app1 = models.TextField(null=True, blank=True)
+    app2 = models.TextField(null=True, blank=True)
+    app3 = models.TextField(null=True, blank=True)
+    app4 = models.TextField(null=True, blank=True)
+    github = models.CharField(max_length=100, null=True, blank=True)
 
     objects = models.Manager()
 
